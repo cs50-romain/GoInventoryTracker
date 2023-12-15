eb039d688b056dfa6cf1542ec116a1ea86533aaa
cs50-romain <cs50.git.romain@gmail.com>
2023-12-15 12:18:17 -0500

    Added sale item implementation using stack

---
+++ sale/sale.go
@@ -0,0 +1,66 @@
+package sale
+
+import (
+	"fmt"
+
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
+)
+
+type ItemNode struct {
+	Name	string
+	Price	int
+	next	*ItemNode
+}
+
+type Stack struct {
+	head	*ItemNode
+}
+
+func (s *Stack) Push(item string, price int) {
+	if s.head == nil {
+		s.head = &ItemNode{item, price, nil}
+	} else {
+		curr := s.head
+		for curr.next != nil {
+			curr = curr.next
+		}
+		curr.next = &ItemNode{item, price, nil}
+	}
+}
+
+func (s *Stack) Pop() *ItemNode {
+	if s.head == nil {
+		return nil
+	} else {
+		curr := s.head
+
+		for curr.next != nil {
+			curr = curr.next
+		}
+
+		last_item := curr.next
+		curr.next = nil
+		return last_item
+	}
+}
+
+func (s *Stack) Peek() *ItemNode {
+	if s.head == nil {
+		return nil
+	} else {
+		curr := s.head
+		for curr != nil {
+			curr = curr.next
+		}
+		return curr
+	}
+}
+
+func (s *Stack) Display() {
+	curr := s.head
+
+	for curr != nil {
+		fmt.Printf("Item: %s, Sale: %d", curr.Name, curr.Price)
+		curr = curr.next
+	}
+}
